987ac27d0d35d14ed5cd82fea8a7e3f35024db89
tawon2137 <ziezonn@naver.com>
2017-05-25 15:22:21 +0900

    헤더반영, 레이지로딩 반영 jquery lazyload 제거

--- @com.kinfolk/js/lazyloading.js
+++ @com.kinfolk/js/lazyloading.js
@@ -5,48 +5,90 @@
     }
     function lazyLoading(sectionName, startY) {
 
-        startY = startY ? startY : 400;
-        var sections = document.querySelectorAll(sectionName || 'module');
-        var windowSizeY = window.innerHeight;
+        startY = startY ? startY : 200;
+        var sections = document.querySelectorAll(sectionName || '.module');
+        var windowSizeY = window.innerHeight || document.documentElement.clientHeight;
         var sectionRanges= [];
         for (var i = 0,len = sections.length; i < len; i++) {
             sectionRanges.push({
                 startP: sections[i].offsetTop,
                 endP: sections[i].clientHeight + sections[i].offsetTop,
-                center: sections[i].clientHeight / 2
+                height: sections[i].clientHeight
             });
         }
 
         function sectionImageLoad(section) {
-            if (section.getAttribute('data-load')) return false;
-
-            var imgarr = section.querySelectorAll('img.lazy');
+            if(section.getAttribute('data-loaded')) return false;
+            var imgarr = section.querySelectorAll('img.lazy[data-img-src]');
             var len = imgarr.length;
 
-            for(var i = 0; i < len; i++){
-                var imgElement = imgarr[i];
-                imgElement.className += ' lazy-loaded';
+            for(var i = 0; i < len; i++) {
+               var src = imgarr[i].getAttribute('data-img-src');
+                if(src){
+                    imgarr[i].setAttribute('src',src);
+                }
             }
-
-            section.setAttribute('data-load', 'true');
+            section.setAttribute('data-loaded', 'true');
         }
 
-        function currentSectionShow(e) {
-            var currentScrollYstart = position() + (windowSizeY / 2);
+        function currentSectionShow() {
+            var currentScrollYstart = position();
+            var currentScrollYend = position() + windowSizeY + startY;
             for(var i = 0, len = sectionRanges.length; i < len; i++) {
-                var sectionYstart = sectionRanges[i].startP;
-                var sectionYend = sectionRanges[i].endP;
-                if(currentScrollYstart < sectionYend  && currentScrollYstart > sectionYstart) {
-                    sectionImageLoad(sections[i]);
+                if(sectionRanges[i]) {
+                    var sectionYstart = sectionRanges[i].startP;
+                    var sectionYend = sectionRanges[i].endP;
+                    var sectionHeight =  sectionRanges[i].height;
+                    if(currentScrollYstart >= sectionYstart && currentScrollYstart <= sectionYend || currentScrollYend >= sectionYstart && currentScrollYend <= sectionYend || windowSizeY > sectionHeight && currentScrollYstart <= sectionYstart && currentScrollYend >= sectionYend) {
+                        sectionImageLoad(sections[i]);
+                    }
                 }
             }
         }
+
+
+
+
+        function loadImage (el, fn, i) {
+            el.className += ' lazy-loaded';
+            fn ? fn(i): null;
+        }
+
+        function elementInViewport(el) {
+            var rect = el.getBoundingClientRect();
+
+            return (
+                rect.top    >= 0
+                && rect.left   >= 0
+                && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+            )
+        }
+
+        var images = new Array()
+            , query = document.querySelectorAll('img.lazy')
+            , processScroll = function(){
+                var newImageList = [];
+                for (var i = 0; i < images.length; i++) {
+                    if (elementInViewport(images[i])) {
+                        loadImage(images[i]);
+                    }else {
+                        newImageList.push(images[i]);
+                    }
+                };
+                images = newImageList;
+            };
+        for (var i = 0; i < query.length; i++) {
+            images.push(query[i]);
+        };
+
+        processScroll();
         currentSectionShow();
+        window.addEventListener('scroll',processScroll);
         window.addEventListener('scroll', currentSectionShow);
     }
 
 
-    window.addEventListener('load', function () {
+    window.addEventListener('DOMContentLoaded', function () {
        lazyLoading('.module');
     });
 })()
